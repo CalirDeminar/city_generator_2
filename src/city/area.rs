41a50db79122a628bae409ea9ea6ba3033ff8979
CalirDeminar <edward.firespinner@gmail.com>
2024-06-25 14:09:37 +0100

    area name tweaks

--- src/city/area.rs
+++ src/city/area.rs
@@ -7,4 +7,22 @@ pub mod area {
         pub name: String,
         pub size: usize,
     }
+
+    #[test]
+    fn area_names() {
+        use crate::city::{
+            city::Era,
+            culture::culture::{random_culture, rebalance_dict_for_culture},
+        };
+        use procgen_templater::dictionary::dictionary::build_dictionary_from_folder;
+        let dict = build_dictionary_from_folder("./data_files");
+        let culture = random_culture(&dict, Era::Medieval);
+        let dict2 = rebalance_dict_for_culture(&culture, &dict);
+        for _i in 0..100 {
+            let t = dict2
+                .get_random_template(vec![vec!["AreaName".to_string()]])
+                .unwrap();
+            println!("{}", dict2.render_template_as_title(&t.id).unwrap());
+        }
+    }
 }
